e7e0067ffd83878a50ce04a1b1648385c5a3156b
wangxueqiong <wangxueqiong@tianyancha.com>
2018-10-28 18:56:13 +0800

    appium-webdriver启动app

--- app_webdriver/startApp.py
+++ app_webdriver/startApp.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from appium import webdriver
+import time
 desired_caps = {
     'platformName': 'Android',
     'deviceName': 'W8R0215A16001204',
@@ -9,4 +10,9 @@ desired_caps = {
     #apk的launcherActivity
     'appActivity': 'com.tianyancha.skyeye.activity.SplashActivity'
 }
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+time.sleep(2000);
+driver.findElement("com.tianyancha.skyeye:id/txt_search_copy1","id").click();
+driver.input("//android.widget.EditText[@resource-id='com.tianyancha.skyeye:id/search_input_et' and @text='请输入公司名、人名等关键词']","input","百度");
+driver.findElement("com.tianyancha.skyeye:id/rl_content","id").click();
